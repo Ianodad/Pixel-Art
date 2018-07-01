a11802d3eea1507e06522998eb6f9533f5e20363
Ian Odhiambo <you@example.com>
2018-07-01 21:23:15 +0300

    Added extra function on a click start button for start game
    I created a on click function that hide the start class and show the
    main body.

--- src/js/designs.js
+++ src/js/designs.js
@@ -1,30 +1,20 @@
-// Select color input
+////////////
+//BACK END//
+////////////
 
-// Select size input
-var row, column, color;
-// When size is submitted by the user, call makeGrid()
-$(document).ready(function() {
-    $('#sizePicker').on('click', function(event) {
-        event.preventDefault();
-        row = $('#inputWeight').val();
-        column = $('#inputHeight').val();
-        makeGrid(column, row);
-        // console.log(row + " " + column);
-    });
-});
-
-function makeGrid(height, weight) {
-    $('tr').remove();
+// grid function that takes in two agruments height and width
+function makeGrid(height, width) {
+    $('tr').remove(); // refresh the Dom if there was current tables
 
     // Your code goes here!
 
+    //use nested loop to generate a grid
     for (var x = 1; x <= height; x++) {
         $('#pixelCanvas').append('<tr id=table' + x + '></tr>');
-        for (var y = 1; y <= weight; y++) {
+        for (var y = 1; y <= width; y++) {
             $('#table' + x).append('<td></td>');
         }
     }
-
     // pick color from picker
 
     $('td').click(function addColor() {
@@ -37,3 +27,31 @@ function makeGrid(height, weight) {
         }
     })
 }
+// function to geneate random color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+////////////////
+// FRONT END //
+///////////////
+var row, column, color;
+// When size is submitted by the user, call makeGrid()
+$(document).ready(function() {
+    $('#sizePicker').on('click', function(event) {
+        event.preventDefault();
+        row = $('#inputWidth').val(); // get values of width sets values to row variable
+        column = $('#inputHeight').val(); // get values of height sets value to height variable
+        makeGrid(column, row); // pass the variable into the makegrid function
+        // console.log(row + " " + column);
+
+    });
+    $(".start").click(function() {
+        $(".main").show();
+        $(".start").hide();
+    });
+});
